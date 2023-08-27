5badf3bb8cf42548bbf7a8eb92c9f5bfe2978cc2
ARojasPolanco <advbrrop23@gmail.com>
2023-08-27 18:56:16 -0300

    change name component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-import Pokedex from './pages/pokedex'
+import Pokedex from './pages/Pokedex'
 import Page404 from './pages/Page404'
 import PokemonDetail from './pages/PokemonDetail'
 import PrivateRoutes from './components/auth/PrivateRoutes'
@@ -12,8 +12,8 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route element={<PrivateRoutes />}>
-      <Route path='/pokedex/' element={<Pokedex />} />
-      <Route path='/pokedex/:pokemonId' element={<PokemonDetail />} />
+        <Route path='/pokedex/' element={<Pokedex />} />
+        <Route path='/pokedex/:pokemonId' element={<PokemonDetail />} />
       </Route>
 
       <Route path='*' element={<Page404 />} />
